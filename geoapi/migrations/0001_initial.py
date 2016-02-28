7eaa0b25967bea63ddf31169bc6ca249f121c6e5
chinomng@gmail.com <mgalan@machinalis.com>
2016-02-28 16:08:37 -0300

    add models for provider and area

---
+++ geoapi/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.contrib.gis.db.models.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Area',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=80)),
+                ('area', django.contrib.gis.db.models.fields.PolygonField(srid=4326)),
+                ('price', models.DecimalField(max_digits=8, decimal_places=3)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Provider',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=80)),
+                ('lang', models.CharField(max_length=10)),
+                ('email_address', models.EmailField(max_length=254)),
+                ('phone_number', models.CharField(max_length=10)),
+                ('currency', models.CharField(max_length=3)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='area',
+            name='provider',
+            field=models.ForeignKey(to='geoapi.Provider'),
+        ),
+    ]
--- geoapi/models.py
+++ geoapi/models.py
@@ -1,2 +1,21 @@
 from django.contrib.gis.db import models
 
+
+class Area(models.Model):
+    name = models.CharField(max_length=80)
+    area = models.PolygonField()
+    price = models.DecimalField(max_digits=8, decimal_places=3)
+    provider = models.ForeignKey('Provider')
+
+    def __str__(self):
+        return self.name
+
+class Provider(models.Model):
+    name = models.CharField(max_length=80)
+    lang = models.CharField(max_length=10)
+    email_address = models.EmailField()
+    phone_number = models.CharField(max_length=10)
+    currency = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
